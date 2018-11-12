32d115189a2eff2b385999d410140c3effdb052b
akageun <akageun@gmail.com>
2018-11-12 17:20:18 +0900

    init_project

--- backend/src/main/java/kr/geun/o/app/BackendApplication.java
+++ backend/src/main/java/kr/geun/o/app/BackendApplication.java
@@ -2,11 +2,24 @@ package kr.geun.o.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:8080").allowedMethods("*").allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 }
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="btn">테스트</button>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        msg: ''
+      }
+    },
+    created() {
+      this.getCall();
+    },
+    methods: {
+      getCall: function () {
+        axios.get('/api/test')
+          .then(result => {
+            this.msg = result.data;
+          })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
