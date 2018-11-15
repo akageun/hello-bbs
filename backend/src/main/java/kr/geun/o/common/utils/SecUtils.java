d64b0d325f5c80353ff5bd66ff6629be303c745f
akageun <akageun@gmail.com>
2018-11-16 00:31:54 +0900

    주석 추가.

--- backend/src/main/java/kr/geun/o/common/utils/SecUtils.java
+++ backend/src/main/java/kr/geun/o/common/utils/SecUtils.java
@@ -9,18 +9,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- *
+ * 시큐리티 관련 유틸 클래스
  *
  * @author akageun
  */
 public class SecUtils {
 
+	/**
+	 * Convert
+	 *  - String -> List<GrantedAuthority>
+	 *
+	 * @param authoritiesStr
+	 * @return
+	 */
 	public static List<GrantedAuthority> mapToGrantedAuthorities(String authoritiesStr) {
 		return mapToGrantedAuthorities(Arrays.asList(authoritiesStr.split(",")));
 	}
 
 	/**
-	 *
+	 * Convert
+	 *  - List<String> -> List<GrantedAuthority>
 	 *
 	 * @param authorities
 	 * @return
@@ -29,6 +37,12 @@ public class SecUtils {
 		return authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
 	}
 
+	/**
+	 * 문자열로 가져오기
+	 *
+	 * @param authorities
+	 * @return
+	 */
 	public static String getAuthorities(Collection<? extends GrantedAuthority> authorities) {
 		return authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
 	}
