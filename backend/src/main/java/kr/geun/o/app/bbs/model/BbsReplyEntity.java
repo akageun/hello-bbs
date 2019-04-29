c13b32030721dc6b5f034164fb1fb0dc3705791c
akageun <akageun@gmail.com>
2019-04-29 22:05:38 +0900

    reply 테이블 모델링

--- backend/src/main/java/kr/geun/o/app/bbs/model/BbsReplyEntity.java
+++ backend/src/main/java/kr/geun/o/app/bbs/model/BbsReplyEntity.java
@@ -1,9 +1,74 @@
 package kr.geun.o.app.bbs.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import kr.geun.o.core.constants.CmnConst;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 /**
- *
- *
  * @author akageun
  */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "bbs_reply")
 public class BbsReplyEntity {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long replyId;
+
+    @Column
+    private Long parentArticleId;
+
+    @Column
+    private String statusCd;
+
+    @Lob
+    @Column
+    private String content;
+
+    @Column
+    private String createdUserId;
+
+    @Column
+    private String updatedUserId;
+
+    /**
+     * 생성일시
+     */
+    @JsonFormat(pattern = CmnConst.YMDHMS_READONLY)
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    /**
+     * 수정일시
+     */
+    @JsonFormat(pattern = CmnConst.YMDHMS_READONLY)
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    /**
+     * 무한루프로 인해 override
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
